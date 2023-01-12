dbed9bfefa5aec4d682913c550339f3a26b933d8
Devesh Maurya <deveshmaurya1996@gmail.com>
2023-01-12 10:03:33 +0000

    first commit

---
+++ src/components/CourseItemDetail/index.js
@@ -0,0 +1,18 @@
+const CourseDetailsItem = props => {
+  const {courseItemDetails} = props
+  const {name, description, imageUrl} = courseItemDetails
+
+  return (
+    <li className="course-details-card">
+      <div className="course-details-container">
+        <img className="course-image" src={imageUrl} alt={name} />
+        <div className="course-info-container">
+          <h1 className="course-title">{name}</h1>
+          <p className="course-description">{description}</p>
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export default CourseDetailsItem
